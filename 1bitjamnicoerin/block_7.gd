93c9339918253273f3ca5f6a8a20950a1a1e7b4b
seal7161 <erin.nguyen7160@gmail.com>
2024-10-07 20:12:22 -0700

    more stuff

--- 1bitjamnicoerin/block_7.gd
+++ 1bitjamnicoerin/block_7.gd
@@ -1,20 +1,14 @@
-extends Node2D
+extends StaticBody2D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	set_visible(false)
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
-
-
-func _on_show_arrow(pos: Variant, rot: Variant) -> void:
-	set_visible(true)
-	position = pos
-	rotation = rot
-
-func _on_hide_arrow() -> void:
-	set_visible(false)
+	if Input.is_action_pressed("ui_down"):
+		$CollisionShape2D.set_disabled(true)
+	else:
+		$CollisionShape2D.set_disabled(false)
