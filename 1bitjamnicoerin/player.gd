d33375ddb0290789bf55ddf8049efaa2ce3c61c0
Nico <125397197+mewmewmewmewmewmewmew@users.noreply.github.com>
2024-10-11 06:52:49 -0400

    last minute edits

--- 1bitjamnicoerin/player.gd
+++ 1bitjamnicoerin/player.gd
@@ -1,10 +1,12 @@
 extends CharacterBody2D
 
-@onready var _animated_sprite = $BatSprite2D
+@onready var animated_sprite = $BatSprite2D
+@onready var anim_trans = $"../Transition"
 @export var ACCEL = 10
 @export var SPEED = 100.0
 @export var JUMP_VELOCITY = -300.0
 @export var DASH_SPEED = 300
+
 var jump_f := false #jump flag, allows the player to jump
 var dash_f := false #dash flag, allows the player to dash
 var dash_dir = null #stores the normalized direction pointing to the dash target
@@ -12,12 +14,18 @@ var walljump_f := true #walljump flag, allows the player to jump from a wall cli
 var clingfacing = 0 #player facing for wallclings, should be -1 for left or 1 for right
 var state = "move"
 var invincible = false #for dashing through enemies
+var triggered = false
+var death_timer_triggered = false
+signal fade_out
+
 
 
 func _physics_process(delta: float) -> void:
 	if state == "move":
+		animated_sprite.play("IDLE")
 		#handles dash
 		if Input.is_action_just_pressed("ui_dash") and dash_f == true:
+			$"../FMOD BASIC/Slash".play()
 			collision_mask = 0b0001 #turns one way collisions back of
 			$dash_timer.start() #ends the dash after a certain amount of time
 			velocity = DASH_SPEED * dash_dir
@@ -29,8 +37,10 @@ func _physics_process(delta: float) -> void:
 		else:
 			player_move(delta)
 	if state == "dash":
+		animated_sprite.play("DASH")
 		player_dash(delta)
 	if state == "wall cling":
+		animated_sprite.play("WALLCLING")
 		player_wallcling()
 
 func player_move(delta):
@@ -40,18 +50,20 @@ func player_move(delta):
 		$coyote_timer.stop()
 	# Add the gravity.
 	else:
-		velocity += get_gravity() * delta
+		if state != "dead": 
+			velocity += (get_gravity()/2) * delta
 		if $coyote_timer.is_stopped() == true and jump_f == true:
 			$coyote_timer.start() #the timer event is at the bottom of this script
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_up") and jump_f == true:
+	if Input.is_action_just_pressed("ui_up") and jump_f == true and state != "dead" :
 		velocity.y = JUMP_VELOCITY
+		$"../FMOD BASIC/Jump".play()
 		jump_f = false
 		$coyote_timer.stop()
 
 	#handles horizontal movement
-	if Input.is_action_pressed("ui_left") != Input.is_action_pressed("ui_right"): #make sure only left xor right is pressed
+	if Input.is_action_pressed("ui_left") != Input.is_action_pressed("ui_right") and state != "dead" : #make sure only left xor right is pressed
 		if Input.is_action_pressed("ui_left"): #this is pressing left
 			if velocity.x > 0:
 				velocity.x -= ACCEL*2 #double ACCEL for reverse movement
@@ -106,6 +118,7 @@ func player_dash(delta):
 			velocity = Vector2(0,0)
 			$wallcling_timer.start()
 			$dash_timer.stop()
+			$"../FMOD BASIC/LandWallCling".play()
 		elif x.get_normal() == Vector2(-1, 0):
 			collision_mask = 0b0011 #turns one way collisions back on
 			invincible = false #for dashing through enemies
@@ -116,6 +129,7 @@ func player_dash(delta):
 			velocity = Vector2(0,0)
 			$wallcling_timer.start()
 			$dash_timer.stop()
+			$"../FMOD BASIC/LandWallCling".play()
 		else:
 			velocity = Vector2(0,0)
 
@@ -154,3 +168,26 @@ func _on_dash_timer_timeout() -> void:
 	collision_mask = 0b0011 #turns one way collisions back on
 	invincible = false #for dashing through enemies
 	state = "move"
+
+
+func _on_area_2d_damage_death() -> void:
+	state = "dead"
+	if not triggered:
+		triggered = true
+		fade_out.emit()
+		animated_sprite.play("DEATH")
+		$"../Transition".play("FADEOUT")
+		$"../FMOD BASIC/Die".play()
+		$"../FMOD BASIC/DieMusic".play()
+		$"../FMOD BASIC/Music".stop()
+		$death_timer.start(4)
+		
+func _unhandled_key_input(event):
+	if death_timer_triggered and state == "dead" and event.is_pressed():
+			get_tree().reload_current_scene()
+		# do something...
+	
+
+
+func _on_death_timer_timeout() -> void:
+	death_timer_triggered = true
